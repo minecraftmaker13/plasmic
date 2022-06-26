3f360154f2561ba35d6182741e550e7c4a5f7943
minecraftmaker13 <98362278+minecraftmaker12@users.noreply.github.com>
2022-06-26 14:39:28 -0400

    e

---
+++ src/main/java/net/mcreator/plasmic/init/PlasmicModBlocks.java
@@ -0,0 +1,31 @@
+
+/*
+ *    MCreator note: This file will be REGENERATED on each build.
+ */
+package net.mcreator.plasmic.init;
+
+import net.minecraftforge.registries.RegistryObject;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.level.block.Block;
+
+import net.mcreator.plasmic.block.NovaPlantBlock;
+import net.mcreator.plasmic.PlasmicMod;
+
+public class PlasmicModBlocks {
+	public static final DeferredRegister<Block> REGISTRY = DeferredRegister.create(ForgeRegistries.BLOCKS, PlasmicMod.MODID);
+	public static final RegistryObject<Block> NOVA_PLANT = REGISTRY.register("nova_plant", () -> new NovaPlantBlock());
+
+	@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+	public static class ClientSideHandler {
+		@SubscribeEvent
+		public static void clientSetup(FMLClientSetupEvent event) {
+			NovaPlantBlock.registerRenderLayer();
+		}
+	}
+}
